0de030fdc486c6ba9afab4716d0bf07267985dbc
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2022-09-26 23:25:24 +0500

    ad details page is left

--- assignments/olx-application/home.js
+++ assignments/olx-application/home.js
@@ -1,21 +1,19 @@
 let data = JSON.parse(localStorage.getItem("ads"));
-console.log(data)
+
 
 for(let i = 0; i < data.length; i++){
   var container = document.getElementById("fluid-container");
-
+  
   var ddiv = document.createElement("div");
   ddiv.innerHTML = `
   <div class="card" style="width: 18rem;">
-<img src="${data[i].image}" alt="...">
-<div class="card-body">
-  <h5  id="title" class="card-title">${data[i].adTitle}</h5>
-  <p class="card-text">${data[i].description}</p>
-  <p class="card-text"><b>${data[i].price}</b></p>
-
-  <a href="#" class="btn btn-primary">View Details</a>
-</div>
-</div>
+    <img src="${data[i].image}" alt="...">
+    <div class="card-body">
+    <h5 id="title" class="card-title">${data[i].adTitle}</h5>
+    <p class="card-text">${data[i].description}</p>
+    <p class="card-text"><b>${data[i].price}</b></p>
+    <a href="http://127.0.0.1:5501/details.html/"  class="btn btn-primary">View Details</a>
+  </div>
 `;
 container.appendChild(ddiv)
 
